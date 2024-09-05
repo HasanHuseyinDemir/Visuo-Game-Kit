bcc4449144f7ef62ceac9691ec70faeeb95f1902
HasanHuseyinDemir <majorexo565@gmail.com>
2024-09-06 02:07:45 +0300

    cooldownMixin init

---
+++ Lib/Mixins/cooldown.js
@@ -0,0 +1,29 @@
+function cooldownMixin({ cooldownTime = 0, lastUsed = 0, hooks = {} } = {}) {
+    return {
+            cooldownTime,
+            lastUsed,
+            hooks: { ...hooks },
+
+            isOnCooldown(currentTime) {
+                return currentTime < this.lastUsed + this.cooldownTime;
+            },
+
+            startCooldown(currentTime) {
+                this.lastUsed = currentTime;
+                if (this.hooks.onCooldownStart) {
+                    this.hooks.onCooldownStart({ cooldownTime: this.cooldownTime });
+                }
+            },
+
+            resetCooldown() {
+                this.lastUsed = 0;
+                if (this.hooks.onCooldownReset) {
+                    this.hooks.onCooldownReset();
+                }
+            },
+
+            setCooldownTime(newCooldownTime) {
+                this.cooldownTime = newCooldownTime;
+            }
+        }
+    };
