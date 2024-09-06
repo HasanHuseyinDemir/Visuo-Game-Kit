2a6895793149fdab35fd778284cfc610341deadc
HasanHuseyinDemir <majorexo565@gmail.com>
2024-09-06 21:46:02 +0300

    vectorMixins,customMixins

---
+++ Lib/Mixins/General/Custom/extendedPoint.js
@@ -0,0 +1,67 @@
+import { vectorMixin } from "../../Vectors/pointWHooks.js";
+
+export function mergeVectorOps() {
+    let vec = vectorMixin();
+    const additionalFunctions = {
+        rotate(angle) {
+            const cos = Math.cos(angle);
+            const sin = Math.sin(angle);
+            const newX = this.x * cos - this.y * sin;
+            const newY = this.x * sin + this.y * cos;
+            this.x = newX;
+            this.y = newY;
+            return this;
+        },
+        dot(v) {
+            return this.x * v.x + this.y * v.y;
+        },
+        cross(v) {
+            return this.x * v.y - this.y * v.x;
+        },
+        normalize() {
+            const mag = this.magnitude();
+            if (mag !== 0) {
+                this.x /= mag;
+                this.y /= mag;
+            }
+            return this;
+        },
+        angleBetween(v) {
+            const dotProduct = this.dot(v);
+            const magnitudeA = this.magnitude();
+            const magnitudeB = v.magnitude();
+            return Math.acos(dotProduct / (magnitudeA * magnitudeB));
+        },
+        limit(max) {
+            if (this.magnitude() > max) {
+                this.normalize();
+                this.scale(max);
+            }
+            return this;
+        },
+        toPolar() {
+            const r = this.magnitude();
+            const theta = Math.atan2(this.y, this.x);
+            return { r, theta };
+        },
+        equals(v) {
+            return this.x === v.x && this.y === v.y;
+        },
+        round(decimals = 2) {
+            const factor = Math.pow(10, decimals);
+            this.x = Math.round(this.x * factor) / factor;
+            this.y = Math.round(this.y * factor) / factor;
+            return this;
+        },
+        inverse() {
+            this.x = -this.x;
+            this.y = -this.y;
+            return this;
+        },
+        toJSON() {
+            return { x: this.x, y: this.y };
+        }
+    };
+    Object.assign(vec, additionalFunctions);
+    return vec;
+}
---
+++ Lib/Mixins/Vectors/pointWHooks.js
@@ -0,0 +1,77 @@
+export function vectorMixin(v = {}, onChange) {
+    let _x = v.x ?? 0;
+    let _y = v.y ?? 0;
+
+    function triggerOnChange() {
+        if (typeof hooks.onChange=="function") {
+            hooks.onChange({ x: this.x, y: this.y });
+        }
+    }
+
+    return {
+        hooks: { ...hooks },
+        onChange,
+        get x() {
+            return _x;
+        },
+        set x(value) {
+            if (_x !== value) {
+                _x = value;
+                triggerOnChange.call(this);
+            }
+        },
+        get y() {
+            return _y;
+        },
+        set y(value) {
+            if (_y !== value) {
+                _y = value;
+                triggerOnChange.call(this);
+            }
+        },
+        add({x = 0, y = 0}) {
+            this.x += x;
+            this.y += y;
+            return this;
+        },
+        sub({x = 0, y = 0}) {
+            this.x -= x;
+            this.y -= y;
+            return this;
+        },
+        mult({x = 1, y = 1}) {
+            this.x *= x;
+            this.y *= y;
+            return this;
+        },
+        scale(scalar) {
+            this.x *= scalar;
+            this.y *= scalar;
+            return this;
+        },
+        divide(scalar) {
+            if (scalar !== 0) {
+                this.x /= scalar;
+                this.y /= scalar;
+            }
+            return this;
+        },
+        reset() {
+            this.x = v.x ?? 0;
+            this.y = v.y ?? 0;
+            return this;
+        },
+        lerp({x, y}, t) {
+            const xC = this.x + t * (x - this.x);
+            const yC = this.y + t * (y - this.y);
+            const lerpVector = vectorMixin({ x: xC, y: yC }, this.hooks);
+            return lerpVector;
+        },
+        clone() {
+            return vectorMixin({ x: this.x, y: this.y }, this.hooks);
+        },
+        toString() {
+            return `(${this.x}, ${this.y})`;
+        },
+    };
+}
