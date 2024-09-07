36baaf2c290bcc5e3b42e650c400297841004f2c
HasanHuseyinDemir <majorexo565@gmail.com>
2024-09-07 04:15:47 +0300

    1

---
+++ Lib/Mixins/General/switcher.js
@@ -0,0 +1,29 @@
+function switcherMixin(config) {
+    let prints = config.prints || {};
+    let method = config.method || (() => "default");
+    return {
+        execute(arg) {
+            const methodName = method(arg);
+            const calledMethodName = prints[methodName] ? methodName : "default";
+            if (prints[calledMethodName]) {
+                return prints[calledMethodName](arg);
+            } else {
+                throw new Error(`Method '${calledMethodName}' not found`);
+            }
+        },
+        onPrint(name, func) {
+            prints[name] = func;
+        },
+        setMethod(func) {
+            method = func;
+        },
+        send(name, arg) {
+            const calledName = prints[name] ? name : "default";
+            if (prints[calledName]) {
+                return prints[calledName](arg);
+            } else {
+                throw new Error(`Print function '${calledName}' not found`);
+            }
+        },
+    };
+}
